2ff307dceb14203d178d4c45ef1ab8fec13d3b08
yashkalra7 <yashpal.kalra@kpmg.co.uk>
2025-08-08 12:37:19 +0100

    pushing code

---
+++ blob/blob.go
@@ -0,0 +1,69 @@
+package blob
+
+import (
+	"context"
+	"fmt"
+	"mailtoblob/config"
+	"mailtoblob/logger"
+	"net/url"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/Azure/azure-storage-blob-go/azblob"
+)
+
+const timeout = 180
+
+func UploadFileToAzureBlobStorage(config *config.AzureConfig, address *string, msgBody *string, objectKey string, prefix string) error {
+	// Calculating folder and subfolder
+	currentDate := time.Now().UTC()
+	//dateFolder := currentDate.Format("02-01-2006")
+	hourFolder := currentDate.Format("15")
+	minute := currentDate.Minute()
+
+	var quarter string
+	switch {
+	case minute < 15:
+		quarter = "00"
+	case minute < 30:
+		quarter = "15"
+	case minute < 45:
+		quarter = "30"
+	default:
+		quarter = "45"
+	}
+
+	// Folder structure: <prefix>/<date>/<hour>/<15min>/<objectKey>
+	//blobName := filepath.Join(prefix, dateFolder, hourFolder, quarter, objectKey)
+	blobName := filepath.Join(prefix, hourFolder, quarter, objectKey)
+
+	credential, err := azblob.NewSharedKeyCredential(config.AccountName, config.AccountKey)
+	if err != nil {
+		logger.Log.Printf("[ERROR] %s", err)
+		return fmt.Errorf("failed to create shared key credential: %w", err)
+	}
+
+	pipeline := azblob.NewPipeline(credential, azblob.PipelineOptions{})
+	URL, err := url.Parse(
+		fmt.Sprintf("https://%s.blob.core.windows.net/%s/%s", config.AccountName, config.ContainerName, blobName))
+	if err != nil {
+		logger.Log.Printf("[ERROR] %s", err)
+		return fmt.Errorf("failed to parse blob URL: %w", err)
+	}
+
+	// Create a context with a timeout that will abort the upload if it takes
+	// more than the passed in timeout.
+	ctx := context.Background()
+	ctx, cancelFn := context.WithTimeout(ctx, time.Duration(timeout)*time.Second)
+	defer cancelFn()
+
+	blockBlobURL := azblob.NewBlockBlobURL(*URL, pipeline)
+	reader := strings.NewReader(*msgBody)
+	_, err = azblob.UploadStreamToBlockBlob(ctx, reader, blockBlobURL, azblob.UploadStreamToBlockBlobOptions{})
+	if err != nil {
+		logger.Log.Printf("[ERROR] %s", err)
+		return fmt.Errorf("failed to upload data to blob: %w", err)
+	}
+	return nil
+}
---
+++ logger/logger.go
@@ -0,0 +1,22 @@
+package logger
+
+import (
+	"log"
+	"os"
+
+	"mailtoblob/sysexits"
+)
+
+// Log custom logger
+var Log *log.Logger
+
+func init() {
+	file, err := os.OpenFile("/Users/ykalra8/Desktop/mailtoblob.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		log.Println(err)
+		// let mta know that there is a critical file missing
+		os.Exit(sysexits.EX_OSFILE)
+	}
+
+	Log = log.New(file, "", log.LstdFlags|log.Lshortfile)
+}
